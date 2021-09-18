61794980a4e362b8aa1a35c2361d63b314c18e13
Claire Chang <claires.chang@gmail.com>
2021-09-18 22:29:24 +0000

    Add HSTW alignments and modifications

--- align_algs.py
+++ align_algs.py
@@ -48,7 +48,7 @@ def align_batch(system, querylist, featdir1, featdir2, outdir, n_cores=8, downsa
         
     elif system==6:
         # Baseline 6: NWTW
-#         gamma = 0.346
+        gamma = 0.8
         inputs = alignNW_batch(querylist, featdir1, featdir2, outdir, n_cores, downsample, gamma)
         
     else:
@@ -362,189 +362,193 @@ def alignNW(featfile1, featfile2, downsample, gamma, outfile=None):
     return wp
 
 
-# def align_HPTW_batch(querylist, featdir1, featdir2, outdir, n_cores, steps, weights, gamma, beta, downsample):
+def align_HPTW_batch(querylist, featdir1, featdir2, outdir, n_cores, steps, weights, gamma, beta, downsample):
     
-#     start = time.time()
-#     outdir.mkdir(parents=True, exist_ok=True)
+    start = time.time()
+    outdir.mkdir(parents=True, exist_ok=True)
     
-#     # prep inputs for parallelization
-#     inputs = []
-#     with open(querylist, 'r') as f:
-#         for line in f:
-#             parts = line.strip().split(' ')
-#             assert len(parts) == 2
-#             featfile1 = (featdir1 / parts[0]).with_suffix('.npy')
-#             featfile2 = (featdir2 / parts[1]).with_suffix('.npy')
-#             queryid = os.path.basename(parts[0]) + '__' + os.path.basename(parts[1])
-#             outfile = (outdir / queryid).with_suffix('.pkl')
+    # prep inputs for parallelization
+    inputs = []
+    with open(querylist, 'r') as f:
+        for line in f:
+            parts = line.strip().split(' ')
+            assert len(parts) == 2
+            featfile1 = (featdir1 / parts[0]).with_suffix('.npy')
+            featfile2 = (featdir2 / parts[1]).with_suffix('.npy')
+            queryid = os.path.basename(parts[0]) + '__' + os.path.basename(parts[1])
+            outfile = (outdir / queryid).with_suffix('.pkl')
             
-#             if os.path.exists(outfile):
-#                 print(f"Skipping {outfile}")
-#             else:
-#                 #(featfile1, featfile2, steps, weights, gamma, beta, downsample, outfile = None, verbose = False)
-#                 inputs.append((featfile1, featfile2, steps, weights, gamma, beta, downsample, outfile))
+            if os.path.exists(outfile):
+                print(f"Skipping {outfile}")
+            else:
+                #(featfile1, featfile2, steps, weights, gamma, beta, downsample, outfile = None, verbose = False)
+                inputs.append((featfile1, featfile2, steps, weights, gamma, beta, downsample, outfile))
 
-#     # process files in parallel
-#     # This line of code is very important! If you do not explicitly specify the context
-#     # multiprocessing may stall due to how the library copies over threads from processes
-#     # (the processes inherit locks but not threads! So a lock might be inherited and never unlocked)
-#     # This forces us to create a new process from scratch so we don't have to worry about the lock problem
-#     with multiprocessing.get_context("spawn").Pool(processes = n_cores) as pool:
-#         pool.starmap(align_HPTW, inputs)
-#     print("Time to finish align_HPTW_batch: ", time.time() - start)
-#     return
+    # process files in parallel
+    # This line of code is very important! If you do not explicitly specify the context
+    # multiprocessing may stall due to how the library copies over threads from processes
+    # (the processes inherit locks but not threads! So a lock might be inherited and never unlocked)
+    # This forces us to create a new process from scratch so we don't have to worry about the lock problem
+    with multiprocessing.get_context("spawn").Pool(processes = n_cores) as pool:
+        pool.starmap(align_HPTW, inputs)
+    print("Time to finish align_HPTW_batch: ", time.time() - start)
+    return
 
-# @numba.jit(forceobj=True)
-# def align_HPTW(featfile1, featfile2, steps, weights, c_gamma, c_beta, downsample, outfile = None):
+@numba.jit(forceobj=True)
+def align_HPTW(featfile1, featfile2, steps, weights, c_gamma, c_beta, downsample, outfile = None):
     
-#     # Read in features from feature files
-#     F1 = np.load(featfile1)
-#     F2 = np.load(featfile2)
+    # Read in features from feature files
+    F1 = np.load(featfile1)
+    F2 = np.load(featfile2)
         
-#     #C = 1 - F1[:,0::downsample].T @ F2[:,0::downsample] # cos distance metric
-#     C = 1 - F1[:,0:].T @ F2[:,0:] # cos distance metric
+    #C = 1 - F1[:,0::downsample].T @ F2[:,0::downsample] # cos distance metric
+    C = 1 - F1[:,0:].T @ F2[:,0:] # cos distance metric
         
-#     # Calculate the minimum of each row in pairwise cost matrix C, and calculate the minimum of each column in C.  
-#     # Concatenate the two lists and determine the median of the concatenated list.  
-#     # Then multiply the median by c_gamma.    
-#     min_Crow = np.min(C, axis=1)
-#     min_Ccol = np.min(C, axis=0)
-#     min_array = np.concatenate((min_Crow, min_Ccol))
-#     median_min = np.median(min_array)
-#     gamma = median_min * c_gamma + 1e-9 # this is in case we want to align to ourselves
-#     beta = c_beta * gamma
+    # Calculate the minimum of each row in pairwise cost matrix C, and calculate the minimum of each column in C.  
+    # Concatenate the two lists and determine the median of the concatenated list.  
+    # Then multiply the median by c_gamma.    
+    min_Crow = np.min(C, axis=1)
+    min_Ccol = np.min(C, axis=0)
+    min_array = np.concatenate((min_Crow, min_Ccol))
+    median_min = np.median(min_array)
+    gamma = median_min * c_gamma + 1e-9 # this is in case we want to align to ourselves
+    beta = c_beta * gamma
         
-#     # Run HSTW
-#     D, B = HPTW(C, steps, weights, gamma, beta)
-#     path_v, path_h = backtrace_HPTW(D, B, steps)
+    # Run HSTW
+    D, B = HPTW(C, steps, weights, gamma, beta)
+    path_v, path_h = backtrace_HPTW(D, B, steps)
         
-#     # If output file is specified, save results
-#     if outfile:
-# #         pickle.dump(path_v, open(outfile, 'wb'))
-# #         pickle.dump(np.sort(np.concatenate((path_v, path_h)), axis = 0), open(outfile, 'wb'))
-#         pickle.dump([path_v, path_h], open(outfile, 'wb'))
+    # If output file is specified, save results
+    
+    if outfile:
+        costs_v = []
+        for element in path_v:
+            costs_v.append(C[element[0], element[1]])
+        # don't care about hidden costs -- just uses gamma
+        d = {"wp": [path_v, path_h], "costs": costs_v}
+        pickle.dump(d, open(outfile, 'wb'))
 
-#     return path_v, path_h
+    return path_v, path_h
 
-# @numba.jit(nopython=True, cache=True)
-# def HPTW(C, steps, weights, gamma, beta):
-#     D = np.zeros((2, C.shape[0], C.shape[1]))
-#     B = np.zeros((2, C.shape[0], C.shape[1]), dtype = np.int8)
+@numba.jit(nopython=True, cache=True)
+def HPTW(C, steps, weights, gamma, beta):
+    D = np.zeros((2, C.shape[0], C.shape[1]))
+    B = np.zeros((2, C.shape[0], C.shape[1]), dtype = np.int8)
     
-#     # 0 = hidden, 1 = visible
+    # 0 = hidden, 1 = visible
     
-#     # Initialize
-#     D[0, 0, 0] = 0
-#     D[1, 0, 0] = np.inf
+    # Initialize
+    D[0, 0, 0] = 0
+    D[1, 0, 0] = np.inf
     
-#     for row in range(C.shape[0]):
-#         for col in range(C.shape[1]):
+    for row in range(C.shape[0]):
+        for col in range(C.shape[1]):
             
-#             # Initialize
-#             if row == 0 and col == 0:
-#                 D[0, 0, 0] = 0
-#                 D[1, 0, 0] = np.inf
-#                 continue
+            # Initialize
+            if row == 0 and col == 0:
+                D[0, 0, 0] = gamma
+                D[1, 0, 0] = C[0,0]
+                continue
                 
-#             # Hidden (assuming we only have three defined steps)
-#             # 0: D(0,i-1,j) + gamma (vertical)
-#             # 1: D(0,i,j-1) + gamma (horizontal)
-#             # 2: D(1,i-1,j-1) + 2*gamma (switch planes, visible to hidden)
+            # Hidden (assuming we only have three defined steps)
+            # 0: D(0,i-1,j) + gamma (vertical)
+            # 1: D(0,i,j-1) + gamma (horizontal)
+            # 2: D(1,i-1,j-1) + 2*gamma (switch planes, visible to hidden)
                                     
-#             # We also need to check for out of bounds!
-#             if row < 1:
-#                 D[0, row, col] = D[0, row, col-1] + gamma
-#                 B[0, row, col] = 1
-#             elif col < 1:
-#                 D[0, row, col] = D[0, row-1, col] + gamma
-#                 B[0, row, col] = 0
-#             else:
-#                 costs_h = np.array([D[0, row-1, col] + gamma, D[0, row, col-1] + gamma, D[1, row-1, col-1] + 2*gamma])
-#                 D[0, row, col] = np.min(costs_h)
-#                 B[0, row, col] = np.argmin(costs_h)
+            # We also need to check for out of bounds!
+            if row < 1:
+                D[0, row, col] = D[0, row, col-1] + gamma
+                B[0, row, col] = 1
+            elif col < 1:
+                D[0, row, col] = D[0, row-1, col] + gamma
+                B[0, row, col] = 0
+            else:
+                costs_h = np.array([D[0, row-1, col] + gamma, D[0, row, col-1] + gamma, D[1, row-1, col-1] + 2*gamma])
+                D[0, row, col] = np.min(costs_h)
+                B[0, row, col] = np.argmin(costs_h)
             
             
-#             # Visible (variable steps and weights)
-#             # Check which step has the minimum cost associated with it
-#             mincost = np.inf
-#             minidx = -1
-#             for stepidx, step in enumerate(steps):
-#                 (rstep, cstep) = step
-#                 prevrow = row - rstep
-#                 prevcol = col - cstep
+            # Visible (variable steps and weights)
+            # Check which step has the minimum cost associated with it
+            mincost = np.inf
+            minidx = -1
+            for stepidx, step in enumerate(steps):
+                (rstep, cstep) = step
+                prevrow = row - rstep
+                prevcol = col - cstep
                 
-#                 if prevrow >= 0 and prevcol >= 0:
-#                     if D[1, prevrow, prevcol] + C[row, col] * weights[stepidx] < mincost:
-#                         mincost = D[1, prevrow, prevcol] + C[row, col] * weights[stepidx]
-#                         minidx = stepidx
+                if prevrow >= 0 and prevcol >= 0:
+                    if D[1, prevrow, prevcol] + C[row, col] * weights[stepidx] < mincost:
+                        mincost = D[1, prevrow, prevcol] + C[row, col] * weights[stepidx]
+                        minidx = stepidx
                         
-#             # Also check option to come up from hidden plane to visible plane
-#             # Cost from Hidden to Visible: D(0,i,j) + beta 
-#             if D[0, row, col] + beta  < mincost:
-#                 mincost = D[0, row, col] + beta 
-#                 minidx = len(steps) # Reserve len(steps) as switch from hidden to visible plane
+            # Also check option to come up from hidden plane to visible plane
+            # Cost from Hidden to Visible: D(0,i,j) + beta 
+            if D[0, row, col] + beta  < mincost:
+                mincost = D[0, row, col] + beta 
+                minidx = len(steps) # Reserve len(steps) as switch from hidden to visible plane
                 
-#             D[1, row, col] = mincost
-#             B[1, row, col] = minidx
+            D[1, row, col] = mincost
+            B[1, row, col] = minidx
             
     
-#     return D, B
+    return D, B
 
-# #@numba.jit(nopython=True, cache=True)
-# def backtrace_HPTW(D, B, steps):
-#     # Hidden = 0, Visible = 1
+#@numba.jit(nopython=True, cache=True)
+def backtrace_HPTW(D, B, steps):
+    # Hidden = 0, Visible = 1
     
-#     row = D.shape[1] - 1
-#     col = D.shape[2] - 1
+    row = D.shape[1] - 1
+    col = D.shape[2] - 1
     
-#     # Start in plane which has lowest cost
-#     starting_cost = np.array([D[0, row, col], D[1, row, col]])
-#     plane = np.argmin(starting_cost)
+    # Start in plane which has lowest cost
+    starting_cost = np.array([D[0, row, col], D[1, row, col]])
+    plane = np.argmin(starting_cost)
     
-#     path_v = []
-#     path_h = []
+    path_v = []
+    path_h = []
     
-#     while row > 0 and col > 0:
+    while row > 0 and col > 0:
         
-#         # Record row and column in path
-#         if plane == 0:
-#             path_h.append([row, col])
-#         else:
-#             path_v.append([row, col])
+        # Record row and column in path
+        if plane == 0:
+            path_h.append([row, col])
+        else:
+            path_v.append([row, col])
             
-#         # Backtrace
+        # Backtrace
         
-#         # Hidden plane (assuming we only have three defined steps)
-#         # 0: D(0,i-1,j) + gamma (vertical)
-#         # 1: D(0,i,j-1) + gamma (horizontal)
-#         # 2: D(1,i-1,j-1) + 2*gamma (switch planes, visible to hidden)
-#         if plane == 0:
-#             if B[plane, row, col] == 0: # Move back one row
-#                 row -= 1
-#             elif B[plane, row, col] == 1: # Move back one column
-#                 col -= 1
-#             else: # Move to visible plane and back one row and col
-#                 plane = 1
-#                 row -= 1
-#                 col -= 1
+        # Hidden plane (assuming we only have three defined steps)
+        # 0: D(0,i-1,j) + gamma (vertical)
+        # 1: D(0,i,j-1) + gamma (horizontal)
+        # 2: D(1,i-1,j-1) + 2*gamma (switch planes, visible to hidden)
+        if plane == 0:
+            if B[plane, row, col] == 0: # Move back one row
+                row -= 1
+            elif B[plane, row, col] == 1: # Move back one column
+                col -= 1
+            else: # Move to visible plane and back one row and col
+                plane = 1
+                row -= 1
+                col -= 1
                 
-#         # Visible plane 
-#         # idx corrresponds to steps specified in steps array
-#         # last idx corresponds to switching planes
-#         else:
-#             stepidx = B[plane, row, col]
+        # Visible plane 
+        # idx corrresponds to steps specified in steps array
+        # last idx corresponds to switching planes
+        else:
+            stepidx = B[plane, row, col]
             
-#             if stepidx == len(steps): # Switch planes
-#                 plane = 0
-#             else:
-#                 #print(steps[stepidx])
-#                 (rstep, cstep) = steps[stepidx]
-#                 row = row - rstep
-#                 col = col - cstep
+            if stepidx == len(steps): # Switch planes
+                plane = 0
+            else:
+                #print(steps[stepidx])
+                (rstep, cstep) = steps[stepidx]
+                row = row - rstep
+                col = col - cstep
                 
-#     if plane == 0:
-#         path_h.append([row, col])
-#     else:
-#         path_v.append([row, col])
+    if plane == 0:
+        path_h.append([row, col])
+    else:
+        path_v.append([row, col])
                 
-#     return np.flipud(path_v), np.flipud(path_h)
+    return np.flipud(path_v), np.flipud(path_h)
